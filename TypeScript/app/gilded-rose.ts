519e2cb7ed681217d160576652af7158aab140dd
nahidDeveloper <sikederahmed@gmail.com>
2024-02-24 22:17:33 +0000

    Added one check for Sulfuras as that item's value we do not need to make changes and skip to next iteration

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -20,18 +20,20 @@ export class GildedRose {
 
   qualityDegrader(item:Item){
     if(item.quality>0){
-       if (item.name != 'Sulfuras, Hand of Ragnaros') { //Everything decreases in QualiTY EXCEPT Sulfuras
           item.quality = item.quality - 1
         //Decrease twice as fast if it is Item Conjured
         if(item.name =="Conjured" && item.quality != 0){
           item.quality = item.quality - 1
         }
-      }
   }
   }
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      //If item is Sulfuras we can continue as there are no changes to this item
+      if(this.items[i].name === 'Sulfuras, Hand of Ragnaros'){
+        continue;
+      }
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {//Aged brie and backpass concert does not decrease
           this.qualityDegrader(this.items[i])   
       } else {// This will run if its Aged Brie or Backstage
@@ -49,9 +51,9 @@ export class GildedRose {
           
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        //Decrease all items sellin Value
         this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+      
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
