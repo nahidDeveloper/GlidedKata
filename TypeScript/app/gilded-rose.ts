d527504512300606d7b925414084fb7eff89c654
nahidDeveloper <sikederahmed@gmail.com>
2024-02-24 17:09:49 +0000

    Extracted repeated of degrading item quality into a seperate method

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,20 +18,22 @@ export class GildedRose {
     this.items = items;
   }
 
-  
+  qualityDegrader(item:Item){
+    if(item.quality>0){
+       if (item.name != 'Sulfuras, Hand of Ragnaros') { //Everything decreases in QualiTY EXCEPT Sulfuras
+          item.quality = item.quality - 1
+        //Decrease twice as fast if it is Item Conjured
+        if(item.name =="Conjured" && item.quality != 0){
+          item.quality = item.quality - 1
+        }
+      }
+  }
+  }
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {//Aged brie and backpass concert does not decrease
-        if (this.items[i].quality > 0) {// Items cannot be below 0
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') { //Everything decreases in QualiTY EXCEPT Sulfuras
-            this.items[i].quality = this.items[i].quality - 1
-            //Decrease twice as fast if it is Item Conjured
-            if(this.items[i].name =="Conjured" && this.items[i].quality != 0){
-              this.items[i].quality = this.items[i].quality - 1
-            }
-          }
-        }
+          this.qualityDegrader(this.items[i])   
       } else {// This will run if its Aged Brie or Backstage
         if (this.items[i].quality < 50) {//No item can be greater than 50
           this.items[i].quality = this.items[i].quality + 1
@@ -53,15 +55,8 @@ export class GildedRose {
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-                // Conjured Decreases twice as normal quality items
-                if(this.items[i].name =="Conjured" && this.items[i].quality != 0){
-                  this.items[i].quality = this.items[i].quality - 1
-                }
-              }
-            }
+              this.qualityDegrader(this.items[i])
+            
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality //Set backstage concert to 0
           }
